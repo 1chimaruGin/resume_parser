190a386a542d1129a1118894347379ad71bdb51b
1chimaruGin <kyi@corpy.co.jp>
2023-10-16 23:50:55 +0630

    updated workers

--- backend/app/app/worker.py
+++ backend/app/app/worker.py
@@ -1,9 +1,13 @@
 import json
-from PIL import Image
+import hashlib
 from raven import Client
+from typing import List
+from sqlalchemy.orm import Session
 from app.core.celery_app import celery_app
 from app.core.config import settings
 from app.lib.match import Matcher
+from app import crud, models, schemas
+
 
 _matcher = Matcher()
 
@@ -13,17 +17,31 @@ def test_celery(word: str) -> str:
     return f"test task return {word}"
 
 @celery_app.task(acks_late=True)
-def process_resume(images: str, job_title: str, industry: str, job_description: str) -> str:
-    resume, simi, records = _matcher.process(images, job_title, industry, job_description)
-    records = json.loads(records)
-    return {
-        "name": records["name"],
-        "resume_text": resume,
-        "score": simi,
-        "records": records,
-        "is_ready": True
-    }
+def process_resume(images: List[str], jd_images: List[str], db: Session, current_user: models.User) -> bool:
+    existing = crud.application.get_multi_by_owner(
+        db=db, owner_id=current_user.id
+    )
+    # Check if the application is already in the database
+    hash_images = hashlib.md5(str(images).encode("utf-8")).hexdigest()
+    hash_jd_images = hashlib.md5(str(jd_images).encode("utf-8")).hexdigest()
+    for ex in existing:
+        hash_ex_resume = hashlib.md5(str(ex.resume).encode("utf-8")).hexdigest()
+        hash_ex_jd = hashlib.md5(str(ex.job_description).encode("utf-8")).hexdigest()
+        if hash_ex_resume == hash_images and hash_ex_jd == hash_jd_images:
+            print("[INFO] The job application is already in the database")
+            return ex
+    
+    # Process the resume
+    resume, job_description, similarity, details = _matcher.process(images, jd_images)
+    details = json.loads(details)
+    details["score"] = similarity
+    obj_in = schemas.ApplicationCreate(resume=resume, job_description=job_description, **details)
 
-@celery_app.task(acks_late=True)
-def send_email(email_to):
-    pass
+    # Create the application
+    crud.application.create_with_owner(
+        db=db, 
+        obj_in=obj_in,
+        owner_id=current_user.id
+    )
+    
+    return True
