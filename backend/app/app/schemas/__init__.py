d7d00e5dc2c2ebb5a9ab6ba438c97eefdfe5317f
1chimaruGin <kyi@corpy.co.jp>
2023-10-17 16:54:21 +0630

    Removed unused imports

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .item import Item, ItemCreate, ItemInDB, ItemUpdate
-from .job import Application, ApplicationCreate, ApplicationInDB, ApplicationUpdate, JobDescription
+from .job import Application, ApplicationCreate, ApplicationUpdate
 from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
