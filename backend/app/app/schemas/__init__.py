e660126efe183f77e7f918d8623884b8d3a5b4c6
1chimaruGin <kyi@corpy.co.jp>
2023-10-26 01:45:38 +0630

    updated schemas user

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -2,4 +2,4 @@ from .item import Item, ItemCreate, ItemInDB, ItemUpdate
 from .job import Application, ApplicationCreate, ApplicationUpdate, ApplicationInDB
 from .msg import Msg
 from .token import Token, TokenPayload
-from .user import User, UserCreate, UserInDB, UserUpdate
+from .user import User, UserCreate, UserInDB, UserUpdate #, UserGroup, UserGroupCreate, UserGroupInDB
--- backend/app/app/schemas/user.py
+++ backend/app/app/schemas/user.py
@@ -1,21 +1,24 @@
 from typing import Optional
-
 from pydantic import BaseModel, EmailStr
+from app.models.enums import RoleType
 
 
 # Shared properties
 class UserBase(BaseModel):
+    user_name: Optional[str] = None
     email: Optional[EmailStr] = None
     is_active: Optional[bool] = True
-    is_superuser: bool = False
+    role: Optional[RoleType] = None
     full_name: Optional[str] = None
-    groups: Optional[str] = None
 
 
 # Properties to receive via API on creation
 class UserCreate(UserBase):
-    email: EmailStr
+    user_name: str
     password: str
+    email: Optional[EmailStr] = None
+    role: Optional[RoleType] = "user"
+    organization: Optional[str] = None
 
 
 # Properties to receive via API on update
@@ -39,9 +42,19 @@ class User(UserInDBBase):
 class UserInDB(UserInDBBase):
     hashed_password: str
 
-
 # class UserGroupBase(BaseModel):
 #     org: Optional[str] = None
+#     user_id: Optional[int] = None
 
 # class UserGroupCreate(UserGroupBase):
 #     org: str
+#     user_id: int
+
+# class UserGroup(UserGroupBase):
+#     id: Optional[int] = None
+
+#     class Config:
+#         orm_mode = True
+
+# class UserGroupInDB(UserGroupBase):
+#     pass
