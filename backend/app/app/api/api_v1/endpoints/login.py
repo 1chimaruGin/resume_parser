38ec6ab6d9630c4822b15e85f1420300d03f615b
1chimaruGin <kyi@corpy.co.jp>
2023-10-27 08:28:56 +0630

    Change request and reesponse

--- backend/app/app/api/api_v1/endpoints/login.py
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -21,28 +21,40 @@ router = APIRouter()
 
 @router.post("/login/access-token", response_model=schemas.Token)
 def login_access_token(
-    db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
+    db: Session = Depends(deps.get_db), login_data: schemas.UserLogin = Body(...)
+    # db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
 ) -> Any:
     """
     OAuth2 compatible token login, get an access token for future requests
     """
     # user = crud.user.authenticate(
-    #     db, email=form_data.username, password=form_data.password
+    #     db, user_name=form_data.username, password=form_data.password
     # )
     user = crud.user.authenticate(
-        db, user_name=form_data.username, password=form_data.password
+        db, user_name=login_data.username, password=login_data.password
     )
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     elif not crud.user.is_active(user):
         raise HTTPException(status_code=400, detail="Inactive user")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    return {
-        "access_token": security.create_access_token(
+    credential = schemas.Token(
+        access_token=security.create_access_token(
             user.id, expires_delta=access_token_expires
         ),
-        "token_type": "bearer",
-    }
+        token_type="bearer",
+        user_name=user.user_name,
+        email=user.email,
+        role=user.role.name,
+        organization=user.organization,
+    )
+    return credential
+    # return {
+    #     "access_token": security.create_access_token(
+    #         user.id, expires_delta=access_token_expires
+    #     ),
+    #     "token_type": "bearer",
+    # }
 
 
 @router.post("/login/test-token", response_model=schemas.User)
